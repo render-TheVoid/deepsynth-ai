04081a90e6d770c9fd30285eac13dfb0ceecb92d
rishabh.sharma@revlabz.ai <rkvmj72@gmail.com>
2026-04-04 11:15:34 +0530

    resolved merge conflicts with auth branch and added typography for structure response

--- client/src/components/Homepage.tsx
+++ client/src/components/Homepage.tsx
@@ -156,18 +156,14 @@ const Homepage = () => {
       <div className='bg-[#101010] ml-[240px] w-[calc(100%-240px)] h-screen flex flex-col mx-auto'>
         {messages.length == 0 && (
           <div className="relative top-25 p-50 text-white/80  font-bold text-center">
-            <h1 className="text-center bg-gradient-to-t from-white/35 to-white/85 text-5xl pb-1 tracking-tighter bg-clip-text text-transparent">{welcome}</h1>
+            <h1 className="text-center bg-linear-to-t from-white/35 to-white/85 text-5xl pb-1 tracking-tighter bg-clip-text text-transparent">{welcome}</h1>
           </div>
         )}
         <div className="chatbox text-white w-full mx-auto space-y-4 pt-10 pb-5 overflow-auto p-[20%] custom-scroll h-full">
           {messages.map((msg, i) => (
             <div
               key={i}
-<<<<<<< HEAD
-              className={`p-2 pl-4 pr-4 rounded-4xl ${msg.type === "user"
-=======
-              className={`p-2 pl-4 pr-4 rounded-4xl prose prose-invert prose-green ${msg.type === "user"
->>>>>>> auth
+              className={`p-2 pl-4 pr-4 rounded-4xl  prose prose-invert prose-green ${msg.type === "user"
                 ? "bg-[#303030] ml-auto text-right shadow-2xl w-fit"
                 : "mr-auto text-left w-180"
                 }`}
